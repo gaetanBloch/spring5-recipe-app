dc1486267d321c3b3a029ac2650c42772a6adae4
gaetan <gaetan.bloch@gmail.com>
2020-03-15 21:50:08 +0100

    Added Repository for Category, Recipe and UnitOfMeasure

---
+++ src/main/java/guru/springframework/spring5recipeapp/repositories/RecipeRepository.java
@@ -0,0 +1,7 @@
+package guru.springframework.spring5recipeapp.repositories;
+
+import guru.springframework.spring5recipeapp.domain.Recipe;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RecipeRepository extends CrudRepository<Recipe, Long> {
+}
